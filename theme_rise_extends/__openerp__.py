31e91285326d4cd1607585a0645c9dfea026f8a0
chang <nobrother@gmail.com>
2017-02-28 21:03:03 +0800

    change

---
+++ theme_rise_extends/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Theme Rise Extends',
+    'description': 'Extend Rise theme for Project EBM',
+    'category': 'Theme/Ecommerce',
+    'version': '1.0',
+    'author': "ohmycode",
+    'depends': [
+        'theme_rise',
+    ],
+    'data': [
+        'view/assets.xml'
+    ],
+}
